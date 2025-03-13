f15aee522b70cb9332688cde89f2614e5610be45
gwooong <gwooong@gmail.com>
2025-03-13 17:20:45 +0900

    modified:   bin/week_2.dart 1단계 수정

--- bin/week_2.dart
+++ bin/week_2.dart
@@ -1,5 +1,48 @@
 import 'package:week_2/week_2.dart' as week_2;
 
+
+
+class Product {
+  String name;
+  int price;
+
+  Product(this.name, this.price);
+}
+
+class ShoppingMall {
+  List<Product> products = [];
+  int totalPrice = 0;
+  
+  void addToCart(Product product) {
+    products.add(product);
+    totalPrice += product.price;
+  }
+
+  void removeToCart(Product product) {
+    products.remove(product);
+    totalPrice -= product.price;
+  }
+
+  void showProducts() {
+    for (var product in products) {
+      print(product.name);
+    }
+  }
+
+  void showTotal() {
+    print('Total price: $totalPrice');
+  }
+}
+
 void main(List<String> arguments) {
-  print('Hello world');
+  List<Product> products = [
+    Product('셔츠', 45000),
+    Product('원피스', 30000),
+    Product('반팔티', 35000),
+    Product('반바지', 38000),
+    Product('양말', 5000),
+  ];
+
+  
+
 }
