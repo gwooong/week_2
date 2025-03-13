1d14e2897c1d130ad995ed562820d6f2f7be6418
gwooong <gwooong@gmail.com>
2025-03-13 17:27:46 +0900

    modified:   bin/week_2.dart

--- bin/week_2.dart
+++ bin/week_2.dart
@@ -32,17 +32,19 @@ class ShoppingMall {
   void showTotal() {
     print('Total price: $totalPrice');
   }
+
 }
 
 void main(List<String> arguments) {
-  List<Product> products = [
-    Product('셔츠', 45000),
-    Product('원피스', 30000),
-    Product('반팔티', 35000),
-    Product('반바지', 38000),
-    Product('양말', 5000),
-  ];
+  var shoppingMall = ShoppingMall();
 
-  
+  shoppingMall.addToCart(Product('셔츠', 45000));
+  shoppingMall.addToCart(Product('원피스', 30000));
+  shoppingMall.addToCart(Product('반팔티', 35000));
+  shoppingMall.addToCart(Product('반바지', 38000));
+  shoppingMall.addToCart(Product('양말', 5000));
+
+  shoppingMall.showProducts();
+  shoppingMall.showTotal();
 
 }
